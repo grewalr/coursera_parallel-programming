c1d9d612a9a75c6c36ae5a40056fe57d6b48ad49
grewalr <ricky@mcgrewal.com>
2016-08-17 19:28:51 +0100

    Implemented box blur kernel

--- wk01-scalashop/src/main/scala/scalashop/package.scala
+++ wk01-scalashop/src/main/scala/scalashop/package.scala
@@ -1,7 +1,7 @@
 
-import common._
 
-package object scalashop {
+package object scalashop
+{
 
   /** The value of every pixel is represented as a 32 bit integer. */
   type RGBA = Int
@@ -19,28 +19,70 @@ package object scalashop {
   def alpha(c: RGBA): Int = (0x000000ff & c) >>> 0
 
   /** Used to create an RGBA value from separate components. */
-  def rgba(r: Int, g: Int, b: Int, a: Int): RGBA = {
+  def rgba(r: Int, g: Int, b: Int, a: Int): RGBA =
+  {
     (r << 24) | (g << 16) | (b << 8) | (a << 0)
   }
 
   /** Restricts the integer into the specified range. */
-  def clamp(v: Int, min: Int, max: Int): Int = {
-    if (v < min) min
-    else if (v > max) max
+  def clamp(v: Int, min: Int, max: Int): Int =
+  {
+    if(v < min) min
+    else if(v > max) max
     else v
   }
 
   /** Image is a two-dimensional matrix of pixel values. */
-  class Img(val width: Int, val height: Int, private val data: Array[RGBA]) {
+  class Img(val width: Int, val height: Int, private val data: Array[RGBA])
+  {
     def this(w: Int, h: Int) = this(w, h, new Array(w * h))
+
     def apply(x: Int, y: Int): RGBA = data(y * width + x)
+
     def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
   }
 
-  /** Computes the blurred RGBA value of a single pixel of the input image. */
-  def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
-    // TODO implement using while loops
-    ???
+  def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA =
+  {
+    // Sum up surrounding pixels then get average
+    // store average in the output pixel
+    var rgbaSum = 0
+    var iter = 0
+    var iterX = clamp(x - radius, 0, src.width)
+
+    while (iterX <= clamp(x + radius, 0, src.width))
+    {
+      var iterY = clamp(y - radius, 0, src.height)
+      while (iterY <= clamp(y + radius, 0, src.height))
+      {
+        rgbaSum += src(iterX, iterY)
+        iterY += 1
+        iter += 1
+      }
+
+      iterX +=1
+    }
+
+    rgbaSum / iter
   }
 
+  /** Computes the blurred RGBA value of a single pixel of the input image. */
+  def boxBlurKernelForLoop(src: Img, x: Int, y: Int, radius: Int): RGBA =
+  {
+    // Sum up surrounding pixels then get average
+    // store average in the output pixel
+    var rgbaSum = 0
+    var iter = 0
+
+    for (i <- clamp(x - radius, 0, src.width) to clamp(x + radius, 0, src.width))
+    {
+      for (j <- clamp(y - radius, 0, src.height) to clamp(y + radius, 0, src.height))
+      {
+        rgbaSum += src(i, j)
+        iter += 1
+      }
+    }
+
+    rgbaSum / iter
+  }
 }
