3f0bf0362fe4c5bb11075221e26cb836ea30a105
Ricky Grewal <RickyGrewal8@gmail.com>
2016-08-28 20:58:59 +0100

    Implemeted parentheis balancing

--- wk02-reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ wk02-reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -15,7 +15,7 @@ object ParallelParenthesesBalancingRunner {
     Key.exec.maxWarmupRuns -> 80,
     Key.exec.benchRuns -> 120,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer new Warmer.Default
 
   def main(args: Array[String]): Unit = {
     val length = 100000000
@@ -36,27 +36,59 @@ object ParallelParenthesesBalancingRunner {
   }
 }
 
-object ParallelParenthesesBalancing {
+object ParallelParenthesesBalancing
+{
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
-   */
-  def balance(chars: Array[Char]): Boolean = {
-    ???
+    */
+  def balance(chars: Array[Char]): Boolean =
+  {
+    def loop(acc: Int, idx: Int): Int =
+    {
+      if (chars.isEmpty || idx == -1) acc
+      else if (idx >= chars.length) chars.length - 1
+      else if (chars(idx) == '(') loop(acc + 1, idx + 1)
+      else if (chars(idx) == ')') loop(acc - 1, idx + 1)
+      else loop(acc, idx + 1)
+    }
+
+    loop(0, 0) == 0
   }
 
-  /** Returns `true` iff the parentheses in the input `chars` are balanced.
-   */
-  def parBalance(chars: Array[Char], threshold: Int): Boolean = {
-
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+  /** Returns `true` if the parentheses in the input `chars` are balanced.
+    */
+  def parBalance(chars: Array[Char], threshold: Int): Boolean =
+  {
+    def traverse(idx: Int, until: Int, totOpenBrace: Int, totCloseBrace: Int) : (Int, Int) =
+    {
+      if (chars.isEmpty || idx >= until) (totOpenBrace, totCloseBrace)
+      else if (chars(idx) == '(') traverse(idx + 1, until, totOpenBrace + 1, totCloseBrace)
+      else if (chars(idx) == ')') traverse(idx + 1, until, totOpenBrace, totCloseBrace + 1)
+      else traverse(idx + 1, until, totOpenBrace, totCloseBrace)
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int) : (Int, Int) =
+    {
+      if(until - from < threshold)
+      {
+        // base case call the traverse function
+        val (open, close) = traverse(from, until, 0, 0)
+        (open, close)
+      }
+      else
+      {
+        val midPoint = from + (until - from) / 2
+
+        val (l, r) = parallel(
+          reduce(from, midPoint -1),
+          reduce(midPoint, until)
+        )
+
+        (l._1 - r._1, l._2 - r._2)
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == (0,0)
   }
 
   // For those who want more:
